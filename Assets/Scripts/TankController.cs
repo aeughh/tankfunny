95ba21e6a745d029a2f82c159ffb504df0c5c613
EvilPunkin <82129788+EvilPunkin@users.noreply.github.com>
2023-06-03 18:13:36 -0700

    why is animating kinematic so awful

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -6,38 +6,60 @@ public class TankController : MonoBehaviour {
     [Header("Rigidbody")]
     [SerializeField] private Rigidbody rigidBody;
 
-    [Header("Treads")]
-    [SerializeField] private GameObject treadPrefab;
-    [SerializeField] private Transform point;
+    // [Header("Treads")]
+    // [SerializeField] private DriveWheel[] driveWheels;
 
-    [SerializeField] private int numberOfTreads;
-    [SerializeField] private float treadSpacingDistance;
-    [SerializeField] private float tensioningSpeed; // unused currently
-
-    [SerializeField] private List<GameObject> treadObjects = new List<GameObject>();
-
-    void Start() {
-        for(int i = 0; i < numberOfTreads; i++) {
-            var spawnPos = point.position + new Vector3(0, i * treadSpacingDistance, 0); // Radius is just the distance away from the point
-
-            GameObject treadInstance = Instantiate(treadPrefab, spawnPos, Quaternion.identity);
-            
-            // treadInstance.transform.LookAt(point.position);
-            treadInstance.transform.Rotate(90, 0, 0);
-
-            treadObjects.Add(treadInstance);
-            
-            Debug.Log(i);
-
-            if(i == 0) {
-                Debug.Log("Wow");
-            } else {
-                treadObjects[i - 1].GetComponent<HingeJoint>().connectedBody = treadObjects[i].GetComponent<Rigidbody>();
-
-                // if(i == numberOfTreads - 1) {
-                //     treadObjects[numberOfTreads - 1].GetComponent<HingeJoint>().connectedBody = treadObjects[0].GetComponent<Rigidbody>();
-                // }
-            }
-        }
+    private void FixedUpdate() {
+        // foreach(var driveWheel in driveWheels) {
+        //     driveWheel.rigidBody.AddRelativeTorque(Vector3.right * driveWheel.torque);
+        // }
     }
 }
+
+[System.Serializable]
+public class DriveWheel {
+    public Rigidbody rigidBody;
+    public float torque;
+}
+
+
+
+
+/*
+for(int i = 0; i < numberOfTreads; i++) {
+    if(i == 0) {
+        Debug.Log("i = 0 V1");
+    } else {
+        treadObjects[i - 1].GetComponent<HingeJoint>().connectedBody = treadObjects[i].GetComponent<Rigidbody>();
+        spawnPos = treadObjects[i - 1].transform.position + new Vector3(0, i * treadSpacingDistance, 0);
+    }
+
+    var x = (int)numberOfTreads / 4;
+
+    if(i <= x) {
+        GameObject treadInstance = Instantiate(treadPrefab, spawnPos, Quaternion.Euler(90, 0, 0));
+        treadObjects.Add(treadInstance);
+    } else if(i <= 2 * x) {
+        GameObject treadInstance = Instantiate(treadPrefab, spawnPos, Quaternion.Euler(0, 0, 0));
+        treadObjects.Add(treadInstance);
+    } else if(i <= 3 * x) {
+        GameObject treadInstance = Instantiate(treadPrefab, spawnPos, Quaternion.Euler(90, 0, 0));
+        treadObjects.Add(treadInstance);
+    } else {
+        GameObject treadInstance = Instantiate(treadPrefab, spawnPos, Quaternion.Euler(0, 0, 0));
+        treadObjects.Add(treadInstance);
+    }
+            
+    // treadInstance.transform.LookAt(point.position);
+
+    if(i == 0) {
+        Debug.Log("i = 0 V2");
+    } else {
+        treadObjects[i - 1].GetComponent<HingeJoint>().connectedBody = treadObjects[i].GetComponent<Rigidbody>();
+
+        // if(i == numberOfTreads - 1) {
+        //     treadObjects[numberOfTreads - 1].GetComponent<HingeJoint>().connectedBody = treadObjects[0].GetComponent<Rigidbody>();
+        // }
+    }
+}
+*/
