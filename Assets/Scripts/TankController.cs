0d45d4ea37bb1132c53deecaad4f17cd2b4c5e9d
EvilPunkin <82129788+EvilPunkin@users.noreply.github.com>
2023-06-05 13:51:06 -0700

    the tank works, finally

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -6,22 +6,96 @@ public class TankController : MonoBehaviour {
     [Header("Rigidbody")]
     [SerializeField] private Rigidbody rigidBody;
 
-    // [Header("Treads")]
-    // [SerializeField] private DriveWheel[] driveWheels;
+    [Header("Treads")]
+    [SerializeField] private Tensioner[] tensioners;
+    [SerializeField] private Axle[] axles;
+    [SerializeField] private LayerMask treadLayerMask;
+    [SerializeField] private Transform leftSprocket;
+    [SerializeField] private Transform rightSprocket;
+    [SerializeField] private float driveWheelRadius;
+    [SerializeField] private float driveForce;
+
+    private void Update() {
+        foreach(var tensioner in tensioners) {
+            // Requires Auto Configure Connected Anchor to be false
+            tensioner.joint.connectedAnchor = Vector3.Lerp(tensioner.joint.connectedAnchor, tensioner.tensionerTargetPosition, tensioner.tensionSpeed);
+        }
+    }
 
     private void FixedUpdate() {
-        // foreach(var driveWheel in driveWheels) {
-        //     driveWheel.rigidBody.AddRelativeTorque(Vector3.right * driveWheel.torque);
-        // }
+        // Left Sprocket
+        Collider[] leftHitColliders = Physics.OverlapSphere(leftSprocket.position + new Vector3(0, -0.25f, 0), driveWheelRadius, treadLayerMask);
+        foreach(var leftCollider in leftHitColliders) {
+            Debug.DrawRay(leftCollider.transform.position, new Vector3(0, driveWheelRadius, 0), Color.green, 0.1f);
+
+            if(Input.GetKey(KeyCode.W)) {
+                leftCollider.GetComponent<Rigidbody>().AddRelativeForce(0, 0, -driveForce);
+            }
+            if(Input.GetKey(KeyCode.S)) {
+                leftCollider.GetComponent<Rigidbody>().AddRelativeForce(0, 0, driveForce);
+            }
+            if(Input.GetKey(KeyCode.A)) {
+                leftCollider.GetComponent<Rigidbody>().AddRelativeForce(0, 0, driveForce);
+            }
+            if(Input.GetKey(KeyCode.D)) {
+                leftCollider.GetComponent<Rigidbody>().AddRelativeForce(0, 0, -driveForce);
+            }
+        }
+        // Right Sprocket
+        Collider[] rightHitColliders = Physics.OverlapSphere(rightSprocket.position + new Vector3(0, -0.25f, 0), driveWheelRadius, treadLayerMask);
+        foreach(var rightCollider in rightHitColliders) {
+            Debug.DrawRay(rightCollider.transform.position, new Vector3(0, driveWheelRadius, 0), Color.green, 0.1f);
+
+            if(Input.GetKey(KeyCode.W)) {
+                rightCollider.GetComponent<Rigidbody>().AddRelativeForce(0, 0, -driveForce);
+            }
+            if(Input.GetKey(KeyCode.S)) {
+                rightCollider.GetComponent<Rigidbody>().AddRelativeForce(0, 0, driveForce);
+            }
+            if(Input.GetKey(KeyCode.A)) {
+                rightCollider.GetComponent<Rigidbody>().AddRelativeForce(0, 0, -driveForce);
+            }
+            if(Input.GetKey(KeyCode.D)) {
+                rightCollider.GetComponent<Rigidbody>().AddRelativeForce(0, 0, driveForce);
+            }
+        }
+
+
+        //axles
+        foreach(var axle in axles) {
+            if(Input.GetKey(KeyCode.W)) {
+                axle.leftWheel.AddRelativeTorque(Vector3.right * axle.wheelForce);
+                axle.rightWheel.AddRelativeTorque(Vector3.right * axle.wheelForce);
+            }
+            if(Input.GetKey(KeyCode.S)) {
+                axle.leftWheel.AddRelativeTorque(Vector3.right * -axle.wheelForce);
+                axle.rightWheel.AddRelativeTorque(Vector3.right * -axle.wheelForce);
+            }
+            if(Input.GetKey(KeyCode.A)) {
+                axle.leftWheel.AddRelativeTorque(Vector3.right * -axle.wheelForce);
+                axle.rightWheel.AddRelativeTorque(Vector3.right * axle.wheelForce);
+            }
+            if(Input.GetKey(KeyCode.D)) {
+                axle.leftWheel.AddRelativeTorque(Vector3.right * axle.wheelForce);
+                axle.rightWheel.AddRelativeTorque(Vector3.right * -axle.wheelForce);
+            }
+        }
     }
 }
 
 [System.Serializable]
-public class DriveWheel {
-    public Rigidbody rigidBody;
-    public float torque;
+public class Axle {
+    public Rigidbody leftWheel;
+    public Rigidbody rightWheel;
+    public float wheelForce;
 }
 
+[System.Serializable]
+public class Tensioner {
+    public ConfigurableJoint joint;
+    public Vector3 tensionerTargetPosition;
+    public float tensionSpeed;
+}
 
 
 
